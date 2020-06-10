e41ba8f59f29c941a1128fee2edb0842b9ac603f
Kevin <21kevinh@tas.tw>
2020-06-11 00:38:53 +0800

    tried to make more efficient

--- prac.cpp
+++ prac.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 
+int FindLocation(int ToFind, vector<int> odds){
+    std::vector<int>::iterator it = std::find(odds.begin(), odds.end(), ToFind);
+    int index = std::distance(odds.begin(), it);
+    return index;
+}
+
 int Solution(vector<int> odds){
-    /*
     int answer;
-    for(int i = 0; (unsigned)i < odds.size(); i+=2){
-        if(odds[i] != odds[i-1] && odds[i] != odds[i+1]){
-            answer = odds[i];
-        }
-    }
-    return answer;
-    */
-   int answer;
+
+   /*
     for(int i = 0; (unsigned)i < odds.size(); i++){
         int res = count(odds.begin(), odds.end(), odds[i]);
         if(res != 2){
@@ -22,10 +22,29 @@ int Solution(vector<int> odds){
         }
 
     }
-    return answer;
+    */
+   vector<int> myset;
+   myset = odds;
+   int i = 2;
+    while(i <= myset.size()){
+        int res = count(myset.begin(), myset.end(), myset[0]);
+        if(res != 2){
+            answer = myset[0];
+            break;
+        }
+        else{
+            int target = myset[0];
+            for(int i = 0; i < 2; i++){
+                int where = FindLocation(target, myset);
+                myset.erase(myset.begin() + where);
+            }   
+        }
+    }
+    return answer; 
+    
 }
 int main(){
-    vector<int> odds = {1, 1, 3, 5, 3, 9, 9, 7 ,5 ,7, 11, 13, 11};
+    vector<int> odds = {1, 1, 3, 5, 3, 9, 9, 7 , 7, 11, 13, 11};
     cout<<Solution(odds);
 
 }
